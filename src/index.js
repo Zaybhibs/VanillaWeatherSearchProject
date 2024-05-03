6f29a226e0129b9c6a5819cbd008068ea17f25d5
zainab olusekun <zaynabolusekun@gmail.com>
2024-05-03 12:48:56 +0100

    apikey and axios added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 
 
+function searchCity (){
+    let apiKey = "563e7044b32f1b708991107c1o5tdea2";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${current-city}&key=${apiKey}&units=metric`;
+}
+
+
 
 
 function searchButton(event){
@@ -7,6 +13,7 @@ function searchButton(event){
     let searchInput = document.querySelector("#search-input");
     let currentCity = document.querySelector("#current-city");
     currentCity.textContent = searchInput.value;
+     searchCity(searchInput.value);
 }
 
 
