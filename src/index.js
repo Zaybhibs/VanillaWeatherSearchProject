07273beb53968e4c705ff19e0685253a4b2ed038
zainab olusekun <zaynabolusekun@gmail.com>
2024-05-15 10:33:54 +0100

    icon fixed

--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ let timeElement = document.querySelector("#current-time");
 let date = new Date(response.data.time*1000);
 timeElement.textContent= formattedDate(date);
 let iconElement = document.querySelector("#icon");
-iconElement.innerHTML = `<img src="${response.data.condition.description.icon_url}" class="current-temperature-icon"/>`;
+console.log(response.data.condition.icon_url)
+iconElement.innerHTML = `<img src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png"  class="current-temperature-icon"/>`
 }
 
 function formattedDate(date){
